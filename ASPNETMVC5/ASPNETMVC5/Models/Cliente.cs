8e10fe9feb080c03e24ca19acd2e0ad9cb798409
Marcelo689 <marcelosj7159@gmail.com>
2022-11-15 21:33:31 -0300

    first commit

---
+++ ASPNETMVC5/ASPNETMVC5/Models/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPNETMVC5.Models
+{
+    public class Cliente 
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+    }
+}
